568931392147edc582043d98ea612af97ba76b66
Ahmed Hussien <ahmedhgabr25@gmail.com>
2025-01-16 19:25:25 +0200

    returned step

--- src/components/DeliveryTimeline.jsx
+++ src/components/DeliveryTimeline.jsx
@@ -2,14 +2,6 @@ import React from "react";
 import { useShipmentContext } from "../hooks/useShipmentContext";
 import styled from "styled-components";
 
-// Define the timeline steps
-const steps = [
-  { label: "Picked Up" },
-  { label: "Processing" },
-  { label: "Out for Delivery" },
-  { label: "Delivered" },
-];
-
 const TimelineWrapper = styled.div`
   display: flex;
   justify-content: space-between;
@@ -22,7 +14,7 @@ const Step = styled.div`
   text-align: center;
   position: relative;
   flex: 1;
-  color: ${({ completed }) => (completed ? "#111619" : "#667085")};
+  color: ${({ completed }) => (completed ? "#009688" : "#ddd")};
 
   &:not(:last-child)::after {
     content: "";
@@ -50,7 +42,8 @@ const Circle = styled.div`
   width: 24px;
   height: 24px;
   border-radius: 50%;
-  background-color: ${({ completed }) => (completed ? "#0098A5" : "white")};
+  background-color: ${({ completed, isReturnedStep }) =>
+    isReturnedStep ? "red" : completed ? "#0098A5" : "white"};
   border: 2px solid ${({ completed }) => (completed ? "#0098A5" : "gray")};
   display: flex;
   align-items: center;
@@ -65,7 +58,8 @@ const Circle = styled.div`
 `;
 
 const Label = styled.span`
-  color: ${({ completed }) => (completed ? "#009688" : "#333")};
+  color: ${({ completed, isReturnedStep }) =>
+    isReturnedStep ? "red" : completed ? "#0098A5" : "white"};
   font-weight: ${({ completed }) => (completed ? "bold" : "normal")};
 `;
 
@@ -79,35 +73,50 @@ function DeliveryTimeline() {
   if (!shipment) return null;
 
   var currentStepIndex = 0;
+  var isCompleted = true;
+  var isReturned = false;
 
   switch (shipment.CurrentStatus.state) {
     case "Received at warehouse":
       currentStepIndex = 0;
       break;
     case "Processing":
+    case "Pending":
       currentStepIndex = 1;
       break;
     case "Out for delivery":
       currentStepIndex = 2;
       break;
-    case "Returned": // TODO: handle returned state
+    case "Returned":
       currentStepIndex = 3;
+      isReturned = true;
       break;
     case "Delivered":
       currentStepIndex = 3;
       break;
   }
+
+  // Define the timeline steps
+  const steps = [
+    { label: "Picked Up" },
+    { label: "Processing" },
+    { label: "Out for Delivery" },
+    { label: isReturned ? "Returned" : "Delivered" },
+  ];
   // date format
   const options = { weekday: "short", month: "short", day: "numeric" };
-  var date = new Date(shipment.CurrentStatus.timestamp)
-  date = date.toLocaleDateString("en-US", options)
+  var date = new Date(shipment.CurrentStatus.timestamp);
+  date = date.toLocaleDateString("en-US", options);
 
   return (
     <TimelineWrapper>
       {steps.map((step, index) => (
         <Step key={step.label} completed={index <= currentStepIndex}>
-          <Circle completed={index <= currentStepIndex} />
-          <Label completed={index <= currentStepIndex}>{step.label}</Label>
+          <Circle
+            completed={index <= currentStepIndex}
+            isReturnedStep={step.label === "Returned"}
+          />
+          <Label completed={index <= currentStepIndex} isReturnedStep = {step.label === "Returned"}>{step.label}</Label>
           {index === currentStepIndex && shipment.CurrentStatus.timestamp && (
             <DateLabel>{date}</DateLabel>
           )}
